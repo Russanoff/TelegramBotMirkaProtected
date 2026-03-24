37fe9a4207fdc62dba425b079803b1333b8b5327
Russanoff <quick.port24@gmail.com>
2026-03-24 16:10:55 +0300

    Bpvtybл меню, добавил гитигнор

--- app/bot/inline_menu/main_menu.py
+++ app/bot/inline_menu/main_menu.py
@@ -1,6 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 main_menu = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='❗️Инструкция', callback_data='vpn_instruction')],
     [InlineKeyboardButton(text="🇳🇱Нидерланды", callback_data="NL")],
     [InlineKeyboardButton(text="🇵🇱Польша", callback_data="PL")],
     [InlineKeyboardButton(text="🇷🇺Россия", callback_data="RU"),
@@ -9,7 +10,6 @@ main_menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="💸Продлить доступ", callback_data="Extend")],
     [InlineKeyboardButton(text='🖥Профиль', callback_data='profile'),
     InlineKeyboardButton(text='⚙️Поддержка', callback_data='support')],
-    [InlineKeyboardButton(text='❗️Инструкция', callback_data='vpn_instruction')],
     #[InlineKeyboardButton(text='Прокси Anty', callback_data='proxy_anty')],
     [InlineKeyboardButton(text='Telegram', callback_data='proxy_tg')]
 ])
