28abcb53e127936a6f03bda2755a0524d20675a5
sergey <sergeyrusanov25@gmail.com>
2026-06-01 13:39:09 +0300

    DeleteOldServers

--- app/apiux/servers.py
+++ app/apiux/servers.py
@@ -14,6 +14,16 @@ SERVERS = {
         "host": "germany.mirkaprotected.ru"
     },
     
+    "PL": {
+        "name": "🇵🇱Poland",
+        "panel_url": "https://89.125.159.161:40184/G7EEoRhzE2XqDTs6Ub/",
+        "username": os.getenv('ADMIN'),
+        "password": os.getenv('PASSWORD'),
+        "remark": "Poland",
+        "sub_base": "https://89.125.159.161:2096/sub/",
+        "host": "89.125.159.161"
+    },
+    
     "LT": {
         "name": " 🇱🇻Latvija",
         "panel_url": "https://ltv.mirkaprotected.ru:4545/vcQi219B8pAO8MeLE4/",
@@ -33,25 +43,6 @@ SERVERS = {
         "sub_base": "https://panel.mirkaprotected.ru:2096/sub/",
         "host": "panel.mirkaprotected.ru"
     },
-
-    "PL": {
-        "name": "🇵🇱Poland",
-        "panel_url": "https://89.125.159.161:40184/G7EEoRhzE2XqDTs6Ub/",
-        "username": os.getenv('ADMIN'),
-        "password": os.getenv('PASSWORD'),
-        "remark": "Poland",
-        "sub_base": "https://89.125.159.161:2096/sub/",
-        "host": "89.125.159.161"
-    },
-
-    # "RU": {
-    #     "name": "Россия",
-    #     "panel_url": "https://130.49.146.87:61143/6t6PYtzf4c9Pp6bT6K/",
-    #     "username": os.getenv('ADMIN'),
-    #     "password": os.getenv('PASSWORD'),
-    #     "remark": "Россия",
-    #     "sub_base": "https://130.49.146.87:2096/sub/"
-    # },
 
     "USA": {
         "name": "🇺🇸USA",
@@ -73,13 +64,4 @@ SERVERS = {
         "host": "france.mirkaprotected.ru"
     },
 
-    # "wlist": {
-    #     "name": "🇺🇸БелыеСписки",
-    #     "panel_url": "https://antbl.mirkaprotected.ru:24020/IQg9HRJn9556hmjiXX/",
-    #     "username": os.getenv('ADMIN'),
-    #     "password": os.getenv('PASSWORD'),
-    #     "remark": "БелыеСписки",
-    #     "sub_base": "https://antbl.mirkaprotected.ru:2096/subbssRibss/",
-    #     "host": "antbl.mirkaprotected.ru"
-    # }
 }
